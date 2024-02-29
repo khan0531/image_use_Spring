1794acf37ec4b96cbb0d372f7d3c6f5b8b383d53
han <rudgksdl94@gmail.com>
2024-03-01 04:34:42 +0900

    스프링이 실행 될 때, 이미지 저장 되는 디렉토리가 자동 생성 되는 코드 구현

--- src/main/java/com/example/image_use_spring/ImageUseSpringApplication.java
+++ src/main/java/com/example/image_use_spring/ImageUseSpringApplication.java
@@ -1,13 +1,36 @@
 package com.example.image_use_spring;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImageUseSpringApplication {
+public class ImageUseSpringApplication implements CommandLineRunner {
+  private final String homeDirectory = System.getProperty("user.home");
+  private final Path imagesDirectory = Paths.get(homeDirectory, "images");
 
   public static void main(String[] args) {
     SpringApplication.run(ImageUseSpringApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    initializeDirectories();
+  }
+
+  private void initializeDirectories() throws IOException {
+    // 필요한 디렉토리 경로 정의
+    Path originalsDir = imagesDirectory.resolve("originals");
+    Path compressesDir = imagesDirectory.resolve("compresses");
+    Path thumbnailsDir = imagesDirectory.resolve("thumbnails");
+
+    // 디렉토리 생성
+    Files.createDirectories(originalsDir);
+    Files.createDirectories(compressesDir);
+    Files.createDirectories(thumbnailsDir);
+  }
 }
