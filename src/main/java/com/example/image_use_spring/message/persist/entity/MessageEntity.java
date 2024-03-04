7a67971948e01a5a2c68a0169e27ee3850b09ece
han <rudgksdl94@gmail.com>
2024-03-04 13:10:48 +0900

    메세지 엔티티

--- src/main/java/com/example/image_use_spring/message/persist/entity/MessageEntity.java
+++ src/main/java/com/example/image_use_spring/message/persist/entity/MessageEntity.java
@@ -1,9 +1,14 @@
 package com.example.image_use_spring.message.persist.entity;
 
+import com.example.image_use_spring.groups.persist.entity.ChatGroupEntity;
+import com.example.image_use_spring.member.persist.entity.MemberEntity;
+import com.example.image_use_spring.message.dto.MessageType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,4 +23,14 @@ public class MessageEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private ChatGroupEntity group;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private MemberEntity sender;
+
+  private String message;
+
+  private MessageType messageType;
 }
