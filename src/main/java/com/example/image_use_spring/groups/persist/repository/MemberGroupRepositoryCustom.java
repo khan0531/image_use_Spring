1556242f3354242fc052b58748051e76c13991b8
han <rudgksdl94@gmail.com>
2024-03-07 01:18:36 +0900

    리펙토링

--- src/main/java/com/example/image_use_spring/groups/persist/repository/MemberGroupRepositoryCustom.java
+++ src/main/java/com/example/image_use_spring/groups/persist/repository/MemberGroupRepositoryCustom.java
@@ -1,10 +1,10 @@
-package com.example.image_use_spring.groups.persist.repository;
-
-import com.example.image_use_spring.groups.persist.entity.ChatGroupEntity;
-import com.example.image_use_spring.groups.persist.entity.MemberGroupEntity;
-import java.time.LocalDate;
-import java.util.List;
-
-public interface MemberGroupRepositoryCustom {
-  List<MemberGroupEntity> findByChatGroupWithCondition(ChatGroupEntity chatGroupEntity, LocalDate joinDate);
-}
+//package com.example.image_use_spring.groups.persist.repository;
+//
+//import com.example.image_use_spring.groups.persist.entity.ChatGroupEntity;
+//import com.example.image_use_spring.groups.persist.entity.MemberGroupEntity;
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//public interface MemberGroupRepositoryCustom {
+//  List<MemberGroupEntity> findByChatGroupWithCondition(ChatGroupEntity chatGroupEntity, LocalDate joinDate);
+//}
--- src/main/java/com/example/image_use_spring/groups/persist/repository/MemberGroupRepositoryImpl.java
+++ src/main/java/com/example/image_use_spring/groups/persist/repository/MemberGroupRepositoryImpl.java
@@ -1,5 +1,28 @@
-package com.example.image_use_spring.groups.persist.repository;
-
-public class MemberGroupRepositoryImpl {
-
-}
+//package com.example.image_use_spring.groups.persist.repository;
+//
+//import com.example.image_use_spring.groups.persist.entity.ChatGroupEntity;
+//import com.example.image_use_spring.groups.persist.entity.MemberGroupEntity;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import java.time.LocalDate;
+//import java.util.List;
+//import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+//
+//public class MemberGroupRepositoryImpl implements MemberGroupRepositoryCustom {
+//
+//  private final JPAQueryFactory queryFactory;
+//  public MemberGroupRepositoryImpl(JPAQueryFactory queryFactory) {
+//    this.queryFactory = queryFactory;
+//  }
+//  public MemberGroupRepositoryImpl() {
+//    super(MemberGroupEntity.class);
+//  }
+//
+//  @Override
+//  public List<MemberGroupEntity> findByChatGroupWithCondition(ChatGroupEntity chatGroupEntity, LocalDate joinDate) {
+//    QMemberGroupEntity qMemberGroup = QMemberGroupEntity.memberGroupEntity;
+//    return from(qMemberGroup)
+//        .where(qMemberGroup.chatGroup.eq(chatGroupEntity)
+//            .and(qMemberGroup.joinDate.after(joinDate)))
+//        .fetch();
+//  }
+//}
