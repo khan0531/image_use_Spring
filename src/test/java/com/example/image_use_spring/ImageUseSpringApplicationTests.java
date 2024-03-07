2b0a9db950ad8cfcf0b87695e9c98eea597eceb0
han <rudgksdl94@gmail.com>
2024-03-07 15:14:52 +0900

    테스트 파일 임시 삭제

--- src/test/java/com/example/image_use_spring/ImageUseSpringApplicationTests.java
+++ src/test/java/com/example/image_use_spring/ImageUseSpringApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.image_use_spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ImageUseSpringApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
-}
+//@SpringBootTest
+//class ImageUseSpringApplicationTests {
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//}
